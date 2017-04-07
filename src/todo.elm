87c48c82cde035d35f81a21e4dc381d7102afad0
Carl Olson <carlolson@users.noreply.github.com>
2017-04-07 16:14:35 -0500

    finish todo example

--- src/todo.elm
+++ src/todo.elm
@@ -1,11 +1,17 @@
 module Todo exposing (..)
 
-import Html exposing (Html, div)
+import Html exposing (Html, div, input, text)
+import Html.Events as Events exposing (onInput, onClick)
+import Html.Attributes exposing (autofocus, id, type_)
+import Json.Decode as Json
+import Dom
+import Task
 
-main = Html.beginnerProgram
-       { model = initModel
+main = Html.program
+       { init = initModel ! []
        , update = update
        , view = view
+       , subscriptions = \x -> Sub.none
        }
 
 type alias Todo =
@@ -16,30 +22,89 @@ type alias Todo =
 
 type alias Model =
     { todos : List Todo
+    , input : String
     }
 
 type Msg
     = Add String
     | Complete Int
+    | Input String
+    | NoOp
 
 initModel : Model
-initModel = Model []
+initModel = Model [] ""
 
-update : Msg -> Model -> Model
+update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
     case msg of
         Add str ->
             { model
                 | todos = (Todo False str <| List.length model.todos) :: model.todos
-            }
+            } ! [ Dom.focus "focus" |> Task.attempt (\_ -> NoOp) ]
+
         Complete n ->
             { model
                 | todos = List.map (\todo ->
                                         if todo.id == n
-                                        then {todo | completed = True}
+                                        then {todo | completed = not todo.completed}
                                         else todo
                                    ) model.todos
-            }
+            } ! []
 
-view : Model -> Html msg
-view model = div [] []
+        Input message ->
+            { model | input = message } ! []
+
+        NoOp ->
+            model ! []
+
+view : Model -> Html Msg
+view model =
+    div [] (viewTodos (List.reverse model.todos) ++
+                [ input [ onInput Input
+                        , onEnter (Add model.input)
+                        , autofocus True
+                        , id "focus" ]
+                      [ text model.input ]
+                , div [] [ text (completedMessage model) ]
+                ])
+
+viewTodo : Todo -> Html Msg
+viewTodo todo =
+    div []
+        [ checkbox (Complete todo.id)
+        , text todo.message ]
+
+viewTodos : List Todo -> List (Html Msg)
+viewTodos todos =
+    case todos of
+        [] -> []
+        todo :: todos ->
+            viewTodo todo :: viewTodos todos
+
+onEnter : Msg -> Html.Attribute Msg
+onEnter msg =
+    let
+        isEnter code =
+            if code == 13 then
+                msg
+            else
+                NoOp
+    in
+        Events.on "keydown" (Json.map isEnter Events.keyCode)
+
+checkbox : Msg -> Html Msg
+checkbox msg =
+    input [ type_ "checkbox"
+          , onClick msg
+          ] []
+
+completedMessage : Model -> String
+completedMessage model =
+    let
+        completed = List.filter (\todo -> todo.completed) model.todos
+        completedCount = List.length completed
+        totalCount = List.length model.todos
+    in
+        if totalCount > 0
+        then (toString completedCount) ++ " / " ++ (toString totalCount) ++ " Completed"
+        else ""
--- tests/Tests.elm
+++ tests/Tests.elm
@@ -25,20 +25,23 @@ all =
               Expect.equal (List.length initModel.todos) 0
         , test "update will add todos" <| \() ->
               let
-                  model = update (Add "hello") initModel
+                  model = justModel <| update (Add "hello") initModel
               in
                   Expect.equal (List.length model.todos) 1
         , test "todos start uncompleted" <| \() ->
               let
-                  model = update (Add "hello") initModel
+                  model = justModel <| update (Add "hello") initModel
               in
                   Expect.true "" <| List.all (\todo -> todo.completed == False) model.todos
         , test "update will complete todos" <| \() ->
               let
                   model = (initModel
                           |> update (Add "hello")
+                          |> justModel
                           |> update (Add "hello")
+                          |> justModel
                           |> update (Complete 0)
+                          |> justModel
                           )
               in
                   case model.todos of
@@ -50,9 +53,68 @@ all =
                       _ -> Expect.fail ""
         , test "gives unique ids to each todo" <| \() ->
             let
-                model = initModel |> update (Add "hello") |> update (Add "hello")
+                model = (initModel
+                        |> update (Add "hello")
+                        |> justModel
+                        |> update (Add "hello")
+                        |> justModel
+                        )
             in
                 case model.todos of
                     t1 :: t2 :: [] -> Expect.notEqual t1.id t2.id
                     _ -> Expect.fail ""
+        , test "view should display todos" <| \() ->
+            let
+                model = initModel |> update (Add "hello") |> justModel
+            in
+                (view model
+                |> Query.fromHtml
+                |> Query.has [ text "hello" ]
+                )
+        , test "input should default to empty" <| \() ->
+            let
+                model = initModel
+            in
+                Expect.equal model.input ""
+        , test "update should update the input" <| \() ->
+            let
+                model = initModel |> update (Input "hello") |> justModel
+            in
+                Expect.equal model.input "hello"
+        , test "input should be rendered" <| \() ->
+            let
+                model = initModel |> update (Input "hello") |> justModel
+            in
+                (view model
+                |> Query.fromHtml
+                |> Query.find [ tag "input" ]
+                |> Query.has [ text "hello" ]
+                )
+        , test "todos should have checkboxes" <| \() ->
+            let
+                model = initModel |> update (Add "hello") |> justModel
+            in
+                (view model
+                |> Query.fromHtml
+                |> Query.has [ attribute "type" "checkbox" ]
+                )
+        , test "shows how many are completed" <| \() ->
+            let
+                model = {todos = [Todo True "" 0, Todo True "" 1, Todo False "" 2], input = ""}
+            in
+                (view model
+                |> Query.fromHtml
+                |> Query.has [ text "2 / 3 Completed" ]
+                )
+        , test "only show completed message if todos exist" <| \() ->
+            (view initModel
+            |> Query.fromHtml
+            |> Query.findAll [ text "Completed" ]
+            |> Query.count (Expect.equal 0)
+            )
         ]
+
+justModel : (Model, Cmd msg) -> Model
+justModel mc =
+    case mc of
+        (model, _) -> model
